272a933f04d34c20e3b011f148189da0e1ea1ccf
Emanuel Umbelino <umbelino.emanuel@gmail.com>
2021-11-09 09:49:07 -0300

    Obter informação do usuário logado e Permitir so administrador criar administrador

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,8 +1,18 @@
-import { Controller, Post, Body, ValidationPipe } from '@nestjs/common';
+import {
+    Controller,
+    Post,
+    Body,
+    ValidationPipe,
+    Get,
+    UseGuards,
+    Req,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from '../users/dto/create-user.dto';
 import { CredentialsDto } from './dto/credentials.dto';
-
+import { AuthGuard } from '@nestjs/passport';
+import { User } from '../users/user.entity';
+import { GetUser } from './get-user.decorator';
 @Controller('auth')
 export class AuthController {
     constructor(private authService: AuthService) { }
@@ -28,5 +38,11 @@ export class AuthController {
             token,
             message: ['Login successfully'],
         };
+    }
+
+    @Get('/me')
+    @UseGuards(AuthGuard())
+    getMe(@GetUser() user: User): User {
+        return user;
     }
 }
--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,13 +1,17 @@
+
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserRepository } from '../users/users.repository';
 import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([UserRepository]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.register({
       secret: 'super-secret',
       signOptions: {
@@ -16,6 +20,7 @@ import { JwtModule } from '@nestjs/jwt';
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
+  exports: [JwtStrategy, PassportModule],
 })
 export class AuthModule { }
